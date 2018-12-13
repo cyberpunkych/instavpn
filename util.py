72e261d942b495ac26c437d73f65edaeeb8affc1
cyberpunkych <cyber-punk@xakep.ru>
2018-12-13 03:29:46 +0300

    Update util.py

--- util.py
+++ util.py
@@ -1,4 +1,14 @@
 import platform, os, logging_subprocess, random, string, logging, sys, json, urllib2, fileinput
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
 
 logger = logging.getLogger()
 
